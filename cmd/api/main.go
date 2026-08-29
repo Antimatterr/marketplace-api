70e54ecc0dcdeb0b3eb66ee069505d3d34432baa
Sidhant Singh <sidhant.singh1+citrix@cloud.com>
2026-08-29 21:01:18 +0530

    configured and added server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
 
 func main() {
-	fmt.Println("build")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status": "OK"}`))
+
+	})
+
+	srv := http.Server{
+		Addr:         ":8001",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed : %v", err)
+	}
 }
